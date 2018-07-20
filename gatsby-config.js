f8e98f00bb53cf75a45510b3beff5669fe68aa83
Baron <baron.zhongyangchen@gmail.com>
2018-07-20 23:59:17 +1000

    first commit

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    siteName: 'Baron cv',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+        omitGoogleFont: true,
+      },
+    }
+  ],
+}

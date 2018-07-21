ee2bd421ed32cb1833ce6a4f4cc572cfe836e35d
Baron <baron.zhongyangchen@gmail.com>
2018-07-21 22:50:03 +1000

    mkae json data available for graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,20 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-typescript`,
+    `gatsby-transformer-json`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography.js`,
         omitGoogleFont: true,
       },
-    }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
   ],
 }
